fb25fae97783b337bfbe69c0b4d0fcfb1f4fea9a
tarun13rathore <tarun13rathore@gmail.com>
2021-07-03 10:36:03 +0530

    date add

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static("public"));
 require("dotenv").config({path: "config.env"})
 const DB = process.env.DATABASE;
 
-const PORT = process.env.PORT || 3007;
+const PORT = process.env.PORT || 3005;
 
 app.use(bodyParser.urlencoded({extended: false})) //Post Body Parser
 //mongoose.connect("mongodb://127.0.0.1:27017/devicedata",{useNewUrlParser: true,useUnifiedTopology: true})
@@ -33,29 +33,6 @@ app.get('/',(req,res)=>{
 })
 
 
-// app.post('/saveData',(req,res)=>{
-//    // res.render('index')
-//     let newData = new Deviceinfo({
-//         deviceid: req.body.deviceid,
-//         batteryvolt: req.body.batteryvolt,
-//         solarvolt: req.body.solarvolt
-
-//     });
-//     newData.save();
-// })
-
-// app.post('/saveData', (req,res,next)=>{
-//      var deviceDeatils = new dataInfo({
-//          deviceid: req.body.deviceid,
-//          batteryvolt: req.body.batteryvolt,
-//          solarvolt: req.body.solarvolt
-//       })
-      //console.log(deviceDeatils);
-     // deviceDeatils.save()
-      //res.redirect('index')
-     //})
-
-
 //dashboard req 
 app.get('/dashboard',(req,res)=>{
     res.render('dashboard')
@@ -64,25 +41,13 @@ app.get('/dashboard',(req,res)=>{
 
 
 
-//important
-
-// app.post('/saveData', (req,res,next)=>{
-//  var deviceDeatils = new dataInfo({
-//     deviceid: req.body.deviceid,
-//     batteryvolt: req.body.batteryvolt,
-//     solarvolt: req.body.solarvolt
-//  })
-//  //console.log(deviceDeatils);
-//  deviceDeatils.save()
-//  //res.redirect('index')
-// })
-
 //get method
 app.get('/saveData',(req,res)=>{
     const deviceDeatils = new dataInfo({
        deviceid: req.query.deviceid,
        batteryvolt: req.query.batteryvolt,
        solarvolt: req.query.solarvolt
+       
     })
     //console.log(deviceDeatils);
     deviceDeatils.save()
@@ -91,25 +56,27 @@ app.get('/saveData',(req,res)=>{
    })
 
 ////
-// app.post('/saveData', (req,res,next)=>{
-//     var deviceDeatils = new dataInfo({
-//        deviceid: req.body.deviceid,
-//        batteryvolt: req.body.batteryvolt,
-//        solarvolt: req.body.solarvolt
-//     })
-//     //console.log(deviceDeatils);
-//     deviceDeatils.save()
-//     //res.redirect('index')
-//    })
+
 
 app.get('/showdata',(req,res)=>{
     //res.render('results')
     dataInfo.find({},(err,results)=>{
         if(err) throw err;
-        console.log(results);
+    //    console.log(results);
         res.render('results',{
             deviceList: results
         })
+    })
+})
+
+
+//indiviual data show in dashboard
+app.get('/indiv',(req,res)=>{
+    dataInfo.findOne({},
+        (err, data1) =>{
+        if(err) throw err;
+        console.log(data1);
+        res.render('data1')
     })
 })
 
--- models/deviceinfo.js
+++ models/deviceinfo.js
@@ -5,7 +5,11 @@ const deviceData = new mongoose.Schema({
     //_id:mongoose.Schema.Types.ObjectId,
     deviceid: String,
     batteryvolt: String,
-    solarvolt: String
+    solarvolt: String,
+    created_at:{
+        type:Date,
+        default:Date.now
+    }
 })
 
 module.exports = mongoose.model('deviceinfo', deviceData)
